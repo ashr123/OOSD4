f441400cb8e80fd2e4be3cdfa0a1203f78387fe5
Saarule <saaryazdi@gmail.com>
2017-06-16 15:48:24 +0300

    level loader works + toolbar

--- src/Game.java
+++ src/Game.java
@@ -11,24 +11,36 @@ class Game extends JFrame
 	private int map;
 	private int numberOfCreeps = 20;
 	private int HP=20;
+	private int waveNumber;
 	private JButton map0;
 	private JButton map1;
 	private JButton map2;
 	private JPanel panel;
+	private JToolBar toolBar;
+	private int time;
 	
 	private Game() throws IOException
 	{
 		super("Tower Defence");
 		loader.load();
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Container contentPane = getContentPane();
+		JToolBar toolBar = new JToolBar();
+		toolBar.add(new JLabel("HP: "+ HP + "    "));
+		toolBar.add(new JLabel("Wave: "+ waveNumber + "    "));
+		toolBar.add(new JLabel("Time: "+ time + "    " ));
+		toolBar.add(new JButton("Fast Forward"));
+		toolBar.add(new JLabel("     "));
+		toolBar.add(new JButton("Go!"));
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		panel = new JPanel();
 		map0 = new JButton("Map 1");
 		map1 = new JButton("Map 2");
 		map2 = new JButton("Map 3");
-		panel = new JPanel();
 		panel.add(map0);
 		panel.add(map1);
 		panel.add(map2);
-		add(panel);
+		add(panel,BorderLayout.CENTER);
 		setSize(800, 800);
 		setResizable(false);
 		setVisible(true);
@@ -37,7 +49,7 @@ class Game extends JFrame
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				map = 1;
+				map = 0;
 				board = new Board(map);
 				getContentPane().add(board);
 			}
@@ -47,7 +59,7 @@ class Game extends JFrame
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				map = 2;
+				map = 1;
 				board = new Board(map);
 				getContentPane().add(board);
 			}
@@ -57,7 +69,7 @@ class Game extends JFrame
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				map = 3;
+				map = 2;
 				board = new Board(map);
 				getContentPane().add(board);
 			}
@@ -77,5 +89,11 @@ class Game extends JFrame
 	public int getMap()
 	{
 		return map;
+	}
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
 	}
 }
